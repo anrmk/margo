5891e662c488ced6b59b2719e1e23eeab4ae2054
Dima <nutella.storch0720@outlook.com>
2020-07-24 13:00:04 -0400

    Issue: #38 Added ServiceDTO and ServiceViewModel

---
+++ Web/ViewModels/Service/ServiceViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Web.ViewModels {
+    public class ServiceViewModel {
+        public long Id { get; set; }
+        public long UccountId { get; set; }
+        public long CategoryId { get; set; }
+    }
+}
