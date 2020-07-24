6e2759f842e6813d93de8461ac974bc556cfc746
Dima <nutella.storch0720@outlook.com>
2020-07-24 16:41:49 -0400

    Issue: #38 Added and Extend Uccount DTOs, Uccount ViewModels

--- Core/Data/Dto/Uccount/UccountDto.cs
+++ Core/Data/Dto/Uccount/UccountDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Core.Data.Dto {
+﻿using System.Collections.Generic;
+using Core.Data.Enums;
+
+namespace Core.Data.Dto {
     public class UccountDto {
         public long Id { get; set; }
         public long CompanyId { get; set; }
         public long VendorId { get; set; }
-        public int Kind { get; set; }
+        public UccountTypes Type { get; set; }
+        public IEnumerable<UccountServiceDto> Services { get; set; }
     }
 }
--- Web/ViewModels/Uccount/UccountViewModel.cs
+++ Web/ViewModels/Uccount/UccountViewModel.cs
@@ -3,5 +3,6 @@
         public long Id { get; set; }
         public long CompanyId { get; set; }
         public long VendorId { get; set; }
+        public int Type { get; set; }
     }
 }
