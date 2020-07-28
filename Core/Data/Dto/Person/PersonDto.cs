3e80fea8445b65aba5c5f05c7453ad0a69a575a2
Fedotov Roman <snickers.storch0720@outlook.com>
2020-07-28 12:33:32 -0400

    Issue 62: Added PersonDto and mapped on PersonEntity

---
+++ Core/Data/Dto/Person/PersonDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Data.Dto
+{
+    public class PersonDto
+    {
+#pragma warning disable CS8618
+#nullable enable
+
+        public long Id { get; set; }
+
+        [StringLength(64, MinimumLength = 1)]
+        public string Name { get; set; }
+
+        [StringLength(64, MinimumLength = 1)]
+        public string SurName { get; set; }
+
+        [StringLength(64)]
+        public string? MiddleName { get; set; }
+
+        [MaxLength(2048)]
+        public string? Description { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+#nullable disable
+#pragma warning disable CS8618
+    }
+}
