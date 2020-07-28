8ff8c2c9de5255af33da266d5b9aecd1758188ad
Fedotov Roman <snickers.storch0720@outlook.com>
2020-07-28 14:55:56 -0400

    Issue 64: Added PersonViews and PersonViewModels

---
+++ Web/ViewModels/Person/PersonListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+#pragma warning disable CS8618
+#nullable enable
+namespace Web.ViewModels
+{
+    public class PersonListViewModel
+    {
+        public long Id { get; set; }
+
+        [StringLength(64, MinimumLength = 1)]
+        public string Name { get; set; }
+
+        [StringLength(64, MinimumLength = 1)]
+        public string SurName { get; set; }
+
+        [StringLength(64)]
+        public string? MiddleName { get; set; }
+
+        [MaxLength(2048)]
+        public string? Description { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
+#nullable disable
+#pragma warning disable CS8618
