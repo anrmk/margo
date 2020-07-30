3a4544e3ca218249329272a8854e26ef96cb1875
Dima <nutella.storch0720@outlook.com>
2020-07-30 14:35:47 -0400

    Issue: #86 Added and extended ViewModels

---
+++ Web/ViewModels/Uccount/UccountVendorFieldViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Data.Enums;
+using Core.Extension;
+
+namespace Web.ViewModels {
+    public class UccountVendorFieldViewModel {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public FieldEnum Type { get; set; }
+        public string TypeName => Type.GetAttribute<DisplayAttribute>().Name;
+        public bool IsRequired { get; set; }
+    }
+}
--- Web/ViewModels/Uccount/UccountViewModel.cs
+++ Web/ViewModels/Uccount/UccountViewModel.cs
@@ -5,12 +5,16 @@ namespace Web.ViewModels {
     public class UccountViewModel {
         public long Id { get; set; }
         public string Name { get; set; }
+        public int Kind { get; set; }
 
         [Display(Name ="Company")]
-        public long CompanyId { get; set; }
+        public long? CompanyId { get; set; }
         [Display(Name = "Vendor")]
-        public long VendorId { get; set; }
-        public int Kind { get; set; }
+        public long? VendorId { get; set; }
+        [Display(Name ="Person")]
+        public long? PersonId { get; set; }
+
         public IEnumerable<UccountServiceViewModel> Services { get; set; }
+        public IEnumerable<UccountVendorFieldViewModel> VendorFields { get; set; }
     }
 }
